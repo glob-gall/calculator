097f29c14ded9106d51f6075ea98a03d8e30e253
glob <luisfelipegalleguillos@hotmail.com>
2020-07-29 22:03:37 -0400

    add framer-motion

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from 'react';
+import { motion } from 'framer-motion'
 
 import { Container,Teste } from './styles';
 
@@ -73,58 +74,100 @@ const Calculator: React.FC = () => {
     setCalculation(0)
     setLastOperator('')
   },[])
-
-
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.05
+      }
+    }
+  }
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  }
 
   return(
     <>
-    <Container>
-      <p>{screen}</p>
-      <button className="operator-grey" onClick={()=>{reset()}}>AC</button>
-      <button 
+    <Container 
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.p
+        variants={item}
+      >{screen}</motion.p>
+      <motion.button 
+      className="operator-grey" onClick={()=>{reset()}}
+      variants={item}
+      >
+        AC
+      </motion.button>
+      <motion.button 
       className="operator-grey" 
       onClick={()=>{invertNumber()}}
+      variants={item}
       >
         +/-
-      </button>
-      <button className="operator-grey">%</button>
-      <button 
+      </motion.button>
+      <motion.button 
+      variants={item}
+      className="operator-grey">%</motion.button>
+      <motion.button 
+      variants={item}
         className="operator-orange"
         onClick={()=>{calculate('/')}}
-      >/</button>
-      <button onClick={()=>addToCalculation(7)}>7</button>
-      <button onClick={()=>addToCalculation(8)}>8</button>
-      <button onClick={()=>addToCalculation(9)}>9</button>
-      <button 
+      >/</motion.button>
+      <motion.button onClick={()=>addToCalculation(7)}
+        variants={item}
+      >7</motion.button>
+      <motion.button onClick={()=>addToCalculation(8)}
+        variants={item}
+      >8</motion.button>
+      <motion.button onClick={()=>addToCalculation(9)}
+        variants={item}
+      >9</motion.button>
+      <motion.button 
+      variants={item}
         className="operator-orange"
         onClick={()=>{calculate('*')}}
       >
         x
-      </button>
-      <button onClick={()=>addToCalculation(4)}>4</button>
-      <button onClick={()=>addToCalculation(5)}>5</button>
-      <button onClick={()=>addToCalculation(6)}>6</button>
-      <button 
+      </motion.button>
+      <motion.button
+        variants={item}
+      onClick={()=>addToCalculation(4)}>4</motion.button>
+      <motion.button variants={item} onClick={()=>addToCalculation(5)}>5</motion.button>
+      <motion.button variants={item} onClick={()=>addToCalculation(6)}>6</motion.button>
+      <motion.button 
+        variants={item}
         className="operator-orange"
         onClick={()=>{calculate('-')}}
-      >-</button>
-      <button onClick={()=>addToCalculation(1)}>1</button>
-      <button onClick={()=>addToCalculation(2)}>2</button>
-      <button onClick={()=>addToCalculation(3)}>3</button>
-      <button 
+      >-</motion.button>
+      <motion.button variants={item} onClick={()=>addToCalculation(1)}>1</motion.button>
+      <motion.button variants={item} onClick={()=>addToCalculation(2)}>2</motion.button>
+      <motion.button variants={item} onClick={()=>addToCalculation(3)}>3</motion.button>
+      <motion.button 
+      variants={item}
         className="operator-orange"
         onClick={()=>{calculate('+')}}
-      >+</button>
-      <button className="zero" onClick={()=>addToCalculation(0)}>0</button>
-      <button onClick={()=>addToCalculation(-1)}>,</button>
-      <button className="operator-orange" onClick={()=>{calculate('=')}}>=</button>
+      >+</motion.button>
+      <motion.button variants={item} className="zero" onClick={()=>addToCalculation(0)}>0</motion.button>
+      <motion.button variants={item} onClick={()=>addToCalculation(-1)}>,</motion.button>
+      <motion.button variants={item} className="operator-orange" onClick={()=>{calculate('=')}}>=</motion.button>
     </Container>
-    <Teste>
+    {/* <Teste>
       <p>result:{result}</p>
       <p>screen:{screen}</p>
       <p>calculation:{calculation}</p>
       <p>lastoperator:{lastOperator}</p>
-    </Teste>
+    </Teste> */}
     </>
   ) 
 }
